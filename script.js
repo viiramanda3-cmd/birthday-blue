647b244a94537b2115070e7f53b43435cb67eb6e
viiramanda3-cmd <viiramanda3@gmail.com>
2026-06-18 01:08:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,23 +8,24 @@ function scrollToMessage(){
 
 }
 
-const particles =
-document.querySelector(".particles");
+const particles = document.querySelector(".particles");
 
-for(let i=0;i<50;i++){
+for(let i=0; i<50; i++){
 
-    let star =
-    document.createElement("div");
+    let star = document.createElement("div");
 
     star.classList.add("particle");
 
-    star.innerHTML="❄";
+    star.innerHTML = "❄";
 
-    star.style.left =
-    Math.random()*100+"vw";
+    star.style.left = Math.random()*100 + "vw";
 
     star.style.animationDuration =
-    (Math.random()*5+5)+"s";
+    (Math.random()*5+5) + "s";
 
     particles.appendChild(star);
+}
+
+function playMusic(){
+    document.getElementById("bgmusic").play();
 }
