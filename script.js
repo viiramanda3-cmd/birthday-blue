41b488f76e5e49e149e65e2e90aea3a5b2b61d40
viiramanda3-cmd <viiramanda3@gmail.com>
2026-06-18 00:38:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function scrollToMessage(){
+
+    document
+    .getElementById("message")
+    .scrollIntoView({
+        behavior:"smooth"
+    });
+
+}
+
+const particles =
+document.querySelector(".particles");
+
+for(let i=0;i<50;i++){
+
+    let star =
+    document.createElement("div");
+
+    star.classList.add("particle");
+
+    star.innerHTML="❄";
+
+    star.style.left =
+    Math.random()*100+"vw";
+
+    star.style.animationDuration =
+    (Math.random()*5+5)+"s";
+
+    particles.appendChild(star);
+}
